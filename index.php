3fbefb07509a8899a7d50c848cfa11a3ea91b77f
buriel <beniahouanto@gmail.com>
2018-10-08 20:12:31 +0300

    This is the first commit

---
+++ index.php
@@ -0,0 +1,281 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <section>
+    <div class="home-bg" style="height: 100vh;">
+      <img src="MACBOOKMOCKUP.jpg" style="top: 0; right: 0; left: 0; bottom: 0; height: 100vh;  width: 100%;">
+      <div class="home-content w-100" style="position: absolute; top: 0; right: 0; bottom: 0; height: 100%; background: rgb(0,0,0,0.6);">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark px-5">
+          <a class="navbar-brand font-weight-bold" href="#">JDW</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="#">Skills</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Portfolio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div class="container text-white">
+          <div class="row">
+            <div class="col-md-5 d-none d-md-block">
+              <div class="" style="border-left: 2px solid white; border-right: 2px solid white; border-bottom: 2px solid white; border-bottom-left-radius: 40%; border-bottom-right-radius: 40%;">
+                <div class="p-4" style="margin-bottom: 300px;">
+                  <h2 style="">I'm Mathias HOUNYE</h2>
+                  <h5>Font-end & Back-end developper</h5>
+                </div>
+
+                <div class="pt-4 pb-2" style="border: 1px solid white; border-radius: 100px; width: 100px; margin: auto;">
+                  <p class="px-2">Learn more</p>
+                  <span style="padding-left: 40px;" ><i class="fa fa-chevron-circle-down" aria-hidden="true"></i></span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-7">
+              <div class="row" style="margin-top: 30%;">
+                <h1 class="mx-auto" style="font-size: 60px;">Web developper 2.0</h1>
+              </div>
+              <div class="row">
+                <h2 class="mx-auto">Font-end & Back-end</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container-fluid about-section">
+      <div class="row bg-light" style="padding-top: 70px; padding-bottom: 70px;">
+          <h1 class="mx-auto">HEY!</h1>
+      </div>
+      <div  class="row">
+        <div class="col-sm-4">
+          <div class="row">
+            <img src="IMG55.jpg" class="img-fluid" style="max-height: 400px;">
+          </div>
+        </div>
+
+        <div class="col-sm-8">
+          <div class="row" style="padding: 50px 100px 40px 50px;">
+            <h2>ABOUT ME</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic </p>
+          </div>
+
+          <div class="row mt-5">
+            <h5 class="pl-5 mr-2">FOLLOW ME</h5>
+            <div class="">
+              <a href="#" class="btn bg-light"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+              <a href="#" class="btn bg-light"><i class="fa fa-google" aria-hidden="true"></i></a>
+              <a href="#" class="btn bg-light"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section>
+    <div class="container-fluid bg-info" style="padding-right: 100px; padding-left: 100px;">
+      <div class="row" style="padding-top: 70px; padding-bottom: 70px;">
+        <div class="card-deck w-100">
+          <div class="card text-center py-4">
+            <i class="fa fa-laptop fa-4x mb-4" aria-hidden="true"></i>
+            <h5>WEB DEVELOPPEMENT</h5>
+          </div>
+
+          <div class="card text-center py-4">
+            <i class="fa fa-server fa-4x mb-4" aria-hidden="true"></i>
+            <h5>WEB SECURITY</h5>
+          </div>
+
+          <div class="card text-center py-4">
+            <i class="fa fa-mobile fa-4x mb-4" aria-hidden="true"></i>
+            <h5>MOBILE-APP</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="container">
+      <div class="row" style="padding-top: 70px; padding-bottom: 70px;">
+        <h2 class="col-12 mb-4 pl-5">MY SKILLS</h2>
+        <div class="col-md-6">
+          <div class="row px-5">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic </p>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="row px-4">
+            <h6>LARAVEL</h6>
+            <div class="progress w-100" style="height: 22px;">
+              <div class="progress-bar bg-info w-50" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
+            </div>
+            <h6>PHP</h6>
+            <div class="progress w-100" style="height: 22px;">
+              <div class="progress-bar bg-info" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+            </div>
+            <h6>JQUERY/JAVASCRIPT</h6>
+            <div class="progress w-100" style="height: 22px;">
+              <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
+            </div>
+            <h6>JAVA</h6>
+            <div class="progress w-100" style="height: 22px;">
+              <div class="progress-bar bg-info" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">100%</div>
+            </div>
+            <h6>CSS3</h6>
+            <div class="progress w-100" style="height: 22px;">
+              <div class="progress-bar bg-info" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
+            </div>
+            <h6>HTML5</h6>
+            <div class="progress w-100" style="height: 22px;">
+              <div class="progress-bar bg-info" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">100%</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid py-5 bg-light">
+      <div class="row">
+        <h1 class="mx-auto">RESUME</h1>
+      </div>
+      <div class="row">
+        <p class="col-sm-6 offset-sm-3 text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic </p>
+      </div>
+      <div class="row">
+        <button class="col-4 offset-4 btn bg-primary font-weight-bold text-white">Download</button>
+      </div>
+    </div>
+  </section>
+
+  <section style="padding-top: 70px; padding-bottom: 70px;">
+    <div class="container">
+      <div class="row">
+        <h2 class="mx-auto pb-5">MY PORTFOLIO</h2>
+      </div>
+      <div class="card-deck">
+        <div class="card">
+          <a href=""><img class="card-img-top" src="portfolio1.jpg" style="height: 150px;"></a>
+        </div>
+        <div class="card">
+          <a href="">
+          <img class="card-img-top" src="portfolio2.jpg" style="height: 150px;"></a>
+        </div>
+        <div class="card"><a href="">
+          <img class="card-img-top" src="portfolio3.jpg" style="height: 150px;"></a>
+        </div>
+        <div class="card">
+          <a href="">
+          <img class="card-img-top" src="portfolio1.jpg" style="height: 150px;"></a>
+        </div>
+      </div>
+
+      <!--<div class="card-deck mt-4">
+        <div class="card">
+          <img class="card-img-top" src="" style="height: 150px;">
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="" style="height: 150px;">
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="" style="height: 150px;">
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="" style="height: 150px;">
+        </div>
+      </div>-->
+    </div>
+  </section>
+
+  <section class="bg-dark text-white" style="padding-top: 70px; padding-bottom: 70px;">
+    <div class="container">
+      <div class="row">
+        <h2 class="mx-auto pb-5">CONTACT ME</h2>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6 bg-info">
+          <div class="p-4">
+            <h5>LEAVE ME A MESSAGE</h5>
+            <form class="col-12" action="" method="">
+              <div class="form-group row">
+                <input type="text" class="form-control" id="name" placeholder="Name*">
+              </div>
+              <div class="form-group row">
+                <input type="email" class="form-control" id="email" placeholder="Email*">
+              </div>
+              <div class="form-group row">
+                <input type="text" class="form-control" id="subject" placeholder="Subject*">
+              </div>
+              <div class="form-group row">
+                <textarea class="form-control" rows="4"  id="message" placeholder="Leave a message*"></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary w-50">SEND</button>
+            </form>
+          </div>
+        </div>
+
+        <div class="col-md-5 offset-md-1">
+          <div class="bg-primary px-4 pb-2">
+              <h5 class="pt-4">ADDRESS</h5>
+              <p>Lorem Ipsum is simply dummy text of </p>
+          </div>
+          <div class="my-3 bg-primary px-4 pb-2">
+              <h5 class="pt-4">PHONE</h5>
+              <p>+380 </p>
+              <p>+380 </p>
+          </div>
+          <div class="bg-primary px-4 pb-2">
+              <h5 class="pt-4">WEB</h5>
+              <p>Mail: </p>
+              <p>Website: </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="container-fluid py-3" style="background: #111;">
+    <p class="text-center text-white">© 2018 - All Rights Reserved.</p>
+  </footer>
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
